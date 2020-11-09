ef08fd41c01d40cec383452885c0568d2eb7edd3
imanmansour86 <imanmansour86@gmail.com>
2020-11-08 21:30:08 -0800

    Createdb and inital migration

--- projects/capstone/starter/app.py
+++ projects/capstone/starter/app.py
@@ -2,15 +2,33 @@ import os
 from flask import Flask, request, abort, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
+from models import setup_db, Movie, Actor
+
 
 def create_app(test_config=None):
-  # create and configure the app
-  app = Flask(__name__)
-  CORS(app)
+    # create and configure the app
+    app = Flask(__name__)
+    setup_db(app)
+    cors = CORS(app, resources={"/*": {"origins": "*", "supports_credentials": True}})
 
-  return app
+    @app.after_request
+    def after_request(response):
+        response.headers.add(
+            "Access-Control-Allow-Headers", "Content-Type,Authorization,true"
+        )
+        response.headers.add(
+            "Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS"
+        )
+        return response
+
+    @app.route("/")
+    def test():
+        return "hello"
+
+    return app
+
 
 APP = create_app()
 
-if __name__ == '__main__':
-    APP.run(host='0.0.0.0', port=8080, debug=True)
+if __name__ == "__main__":
+    APP.run(host="0.0.0.0", port=8080, debug=True)
--- projects/capstone/starter/models.py
+++ projects/capstone/starter/models.py
@@ -1,1 +1,102 @@
+import os
+from sqlalchemy import Column, String, Integer, DateTime, ForeignKey
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+import json
 
+database_name = "capstone"
+database_path = "postgres://{}/{}".format("localhost:5432", database_name)
+
+db = SQLAlchemy()
+migrate = Migrate()
+
+"""
+setup_db(app)
+    binds a flask application and a SQLAlchemy service
+"""
+
+
+def setup_db(app, database_path=database_path):
+    app.config["SQLALCHEMY_DATABASE_URI"] = database_path
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    db.app = app
+    db.init_app(app)
+    migrate = Migrate(app, db)
+
+
+"""
+Movies
+"""
+
+
+class Movie(db.Model):
+    __tablename__ = "movies"
+
+    id = Column(Integer, primary_key=True)
+    title = Column(String)
+    release_date = Column(DateTime)
+    actors = db.relationship("Actor", backref="movies", lazy=True)
+
+    def __init__(self, title, release_date):
+        self.title = title
+        self.release_date = release_date
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def format(self):
+        return {
+            "id": self.id,
+            "title": self.title,
+            "release_date": self.release_date,
+            "actors": self.actors,
+        }
+
+
+"""
+Actors
+"""
+
+
+class Actor(db.Model):
+    __tablename__ = "actors"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    age = Column(Integer)
+    gender = Column(String)
+    movie_id = Column(Integer, ForeignKey("movies.id"), nullable=False)
+
+    def __init__(self, name, age, gender):
+        self.name = name
+        self.age = age
+        self.gender = gender
+        self.movie_id = movie_id
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def format(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "age": self.age,
+            "gender": self.gender,
+            "movie_id": self.movie_id,
+        }
